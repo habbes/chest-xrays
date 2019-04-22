9e28cf9f9c7c0ef4655e2e593225740800e3bc03
Clément Habinshuti <hello@habbes.xyz>
2019-04-23 01:56:35 +0300

    Create ensemble classifier

--- src/model.py
+++ src/model.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
+import os
+
 from dataset import LABELS
 
 def get_model(pretrained=True, finetune=False, arch="densenet"):
@@ -18,3 +21,48 @@ def get_model(pretrained=True, finetune=False, arch="densenet"):
 
 def get_optimizer(params, **kwargs):
     return optim.Adam(params, **kwargs)
+
+class Ensemble(nn.Module):
+    """
+    Ensemble model, averages outputs
+    from all the individual models.
+    Should only be used for inference
+    """
+    def __init__(self, models):
+        super(Ensemble, self).__init__()
+        self.models = models
+    
+    def forward(self, x):
+        ens = None
+        for model in self.models:
+            y = model(x)
+            y = torch.sigmoid(y)
+            ens = y if ens is None else torch.cat((ens, y), dim=-1)
+        ens = ens.view(-1, len(self.models), len(LABELS))
+        ens = ens.mean(dim=1)
+        return ens
+
+
+def load_models(model_dirs):
+    """
+    load models from all the provided directories.
+    Any file with a .pth extension is considered a model file.
+    """
+    models = []
+    for directory in model_dirs:
+        for filename in directory:
+            if filename.endswith('.pth'):
+                model_path = os.path.join(directory, filename)
+                model = get_model()
+                model.load_state_dict(torch.load(model_path))
+                models.append(model)
+    return models
+
+def load_ensemble_model(model_dirs):
+    """
+    Create ensemble classifier from models
+    stored in the specified directories
+    """
+    models = load_models(model_dirs)
+    ensemble = Ensemble(models)
+    return ensemble
