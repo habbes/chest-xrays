ebd12e89ad899b25fc819cf0034125ed8a966d23
Clément Habinshuti <hello@habbes.xyz>
2019-04-26 11:15:27 +0300

    Debugging

--- src/run_baseline_u0.py
+++ src/run_baseline_u0.py
@@ -4,4 +4,4 @@ from baseline_core import run_baseline
 if __name__ == '__main__':
     print("Baseline model with 0.0 for uncertain")
     output_dir = './models/baseline_u0'
-    run_baseline(output_dir=output_dir, uncertainty_strategy='zero')
+    run_baseline(output_dir=output_dir, uncertainty_strategy='zero', max_train_samples=1)
