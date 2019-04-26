0872b064f8fb4dc2e3f5a7ce03701df7427100e4
Clément Habinshuti <hello@habbes.xyz>
2019-04-26 05:14:42 +0300

    Remove test placeholders

--- src/trainer.py
+++ src/trainer.py
@@ -16,7 +16,7 @@ from model import get_model, get_optimizer, load_ensemble_from_checkpoints
 from util import CheckpointManager, display_elapsed_time
 
 RESULTS_DIR = './results'
-CHECKPOINT_COUNT = 1#2000
+CHECKPOINT_COUNT = 2000
 CHECKPOINTS_PER_RUN = 3
 
 def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=3, max_samples=None):
