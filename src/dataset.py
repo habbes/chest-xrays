c1e9ad0a0b7169086a2e0a9b6329174305b19d59
manuj10 <contact@manujpaliwal.com>
2019-04-27 11:10:06 +0530

    spark

--- src/dataset.py
+++ src/dataset.py
@@ -53,7 +53,7 @@ class TrainingDataset(Dataset):
         return len(self.df.columns)
 
     def __getitem__(self, idx):
-        print(self.df.collect())
+        print(self.df.show())
         img_path = path.join(self.data_dir, self.df.collect()[idx]['Path'])
         print(img_path)
         img = Image.open(img_path).convert('RGB')
